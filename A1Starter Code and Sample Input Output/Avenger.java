0f4eb194e23337c974203b7ba6adf6898b194622
singhmru868 <74209444+singhmru868@users.noreply.github.com>
2023-01-29 14:20:15 -0700

    First Push
    All files with updated changes to A1.java and Avengers.java

---
+++ A1Starter Code and Sample Input Output/Avenger.java
@@ -0,0 +1,62 @@
+import java.util.Objects;
+
+public class Avenger implements Comparable <Avenger>{
+	
+	private String heroName;
+    private String heroAlias;
+    private int frequency;
+
+	// TODO: Implement the Avenger Class
+    
+    public String getHeroName() {
+        return heroName;
+    }
+    
+    public void setHeroName(String heroName) {
+        this.heroName = heroName;
+    }
+    
+    public String getHeroAlias() {
+        return heroAlias;
+    }
+    
+    public void setHeroAlias(String heroAlias) {
+        this.heroAlias = heroAlias;
+    }
+    
+    public int getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
+    }
+    
+	@Override
+	public int compareTo(Avenger other) {
+		
+		return this.heroAlias.compareTo(other.heroAlias);
+	}
+	
+	public boolean equals(Object o) {
+		
+        if (this == o) return true;
+        
+        if (o == null || getClass() != o.getClass()) return false;
+        
+        Avenger avenger = (Avenger) o;
+        
+        return Objects.equals(heroAlias, avenger.heroAlias);
+    {}
+    
+    
+		// TODO Auto-generated method stub
+    public String toString() {
+        return heroAlias + " aka " + heroName + " mentioned " + frequency + " time(s)";
+    }
+    {	
+}
+	}
+
+}
+ 
